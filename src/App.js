b1cdc67dd2337c299d1b0e4fb383411477b1080d
ev <zhenya.bill@gmail.com>
2022-01-14 18:52:54 +0200

    classwork2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from "react";
 
 import Users from "./components/Users/Users";
-// import Posts from "./components/Posts/Posts";
-// import Comments from "./components/Comments/Comments";
 import './App.css';
 
 function App() {
