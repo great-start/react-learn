95b7504ce8dcd7df384379f84b700f962990cb98
ev <zhenya.bill@gmail.com>
2022-01-17 10:55:21 +0200

    Revert "form_useRef"
    This reverts commit 5f9cdb5f7476cc4e1fa765d2299f1b457b1a82c9.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 const Form = () => {
-
-
-
     return (
         <div>
             
--- src/configs/urls.js
+++
@@ -1,7 +0,0 @@
-const baseURL = 'http://91.201.233.14/api/v2/cars';
-
-export default baseURL;
-
-export const url = {
-
-}
