ef8563b3e172528697f9e4e50709c35b3690099a
ev <zhenya.bill@gmail.com>
2022-01-19 15:07:36 +0200

    Routes

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,6 +1,6 @@
 import {axiosService} from "./axios.services";
 import {url} from "../configs/urls";
 
-const userServices = () => {
+export const userServices = () => {
     getAll: () => axiosService.get(url.users).then(response => response.data);
 }
