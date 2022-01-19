a66cf96b4cff7e9d249e9c2b9d5d95c673730580
ev <zhenya.bill@gmail.com>
2022-01-19 10:30:25 +0200

    Master_Base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import Comments from "./components/Comments/Comments";
+
 import './App.css';
 
 function App() {
   return (
       <>
-        <Users/>
-        <Posts/>
-        <Comments/>
+
       </>
   );
 }
