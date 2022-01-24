0a1378b62f82e0481f154f66dbdb2cca14caaeba
ev <zhenya.bill@gmail.com>
2022-01-24 17:10:55 +0200

    Rick and Morty

--- src/Components/Episode/Episode.js
+++ src/Components/Episode/Episode.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
-import css from "./Episode.module.css"
 import {Link} from "react-router-dom";
+
+import css from "./Episode.module.css";
 
 const Episode = ({episodeOne}) => {
 
--- src/Components/Episode/Episodes.js
+++ src/Components/Episode/Episodes.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import {useEffect} from "react";
+import React, {useState, useEffect} from 'react';
 
 import {episodeService} from "../../services/episode.services";
 import css from "./Episode.module.css"
